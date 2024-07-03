6819b1649d24d1253f8c40835bdfbab48b36a361
mingmingmon <mingmingmon@naver.com>
2024-07-03 09:43:28 +0900

    setting: 프로젝트 세팅

---
+++ src/main/java/com/example/techtitansserver/TechTitansServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.techtitansserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTitansServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechTitansServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/techtitansserver/TechTitansServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techtitansserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTitansServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
