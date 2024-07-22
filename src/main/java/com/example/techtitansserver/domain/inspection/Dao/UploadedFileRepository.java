ffa5b92be051f981153e55fa14c5adbbfcecb0c7
mingmingmon <mingmingmon@naver.com>
2024-07-22 15:16:46 +0900

    fix: 테이블 수정 및 불필요한 코드 삭제 #13

--- src/main/java/com/example/techtitansserver/domain/inspection/Dao/UploadedFileRepository.java
+++ src/main/java/com/example/techtitansserver/domain/inspection/Dao/UploadedFileRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UploadedFileRepository extends JpaRepository<InspectionResult, Long> {
 
-    Optional<InspectionResult> findByUrl(String fileUrl);
 
 }
 
---
+++ src/main/java/com/example/techtitansserver/domain/inspection/Domain/Defect.java
@@ -0,0 +1,16 @@
+package com.example.techtitansserver.domain.inspection.Domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Defect {
+
+    OIL("오일"),
+    SCRATCH("스크래치"),
+    STAIN("찍힘"),
+    BLACK_SPOT("흑점");
+
+    private String defectName;
+}
